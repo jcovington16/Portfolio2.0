28b078f5a9782f0b97365279e082e5277222f83a
jcovington16 <covingtonjoshua7@gmail.com>
2021-08-06 16:31:41 -0600

    working heavily on components. ensuring I implement the basic components such as footer ect.

---
+++ my-portfolio/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>Made with ⚡️ by Joshua Covington aka Jcyber2000</footer>
+    )
+}
+
+export default Footer;
+
---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
